9ee3dac9ea5cc54651e5f150b18eeb2c6e935563
vk <vineet.kumar@privado.ai>
2023-12-30 14:54:26 +0400

    fix: escape key and value before passing to Regex

--- src/modules/chats/chat/utils.ts
+++ src/modules/chats/chat/utils.ts
@@ -1,3 +1,5 @@
+import escapeStringRegexp from 'escape-string-regexp';
+
 export const getMaskedHtmlMessage = (
     { message, piiToEntityMapData, isUser, noHighlight = false }:
         { isUser: boolean, message: string; noHighlight?: boolean; piiToEntityMapData: { key: string; label: string }[] }
@@ -5,9 +7,9 @@ export const getMaskedHtmlMessage = (
     let updatedMessage = message
     piiToEntityMapData.forEach((item) => {
         if (isUser) {
-            updatedMessage = updatedMessage.replace(new RegExp(`${item.key}`, "g"), noHighlight ? item.key : `<span class="redact-content" data-tooltip-id="tooltip-${item.key}"}>${item.key}</span>`)
+            updatedMessage = updatedMessage.replace(new RegExp(escapeStringRegexp(item.key), "g"), noHighlight ? item.key : `<span class="redact-content" data-tooltip-id="tooltip-${item.key}"}>${item.key}</span>`)
         } else {
-            updatedMessage = updatedMessage.replace(new RegExp(`<${item.label}>`, "g"), item.key)
+            updatedMessage = updatedMessage.replace(new RegExp(`<${escapeStringRegexp(item.label)}>`, "g"), item.key)
         }
     })
     return updatedMessage
